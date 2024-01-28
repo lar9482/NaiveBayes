191093adbe98b96855f90051d40a12d0a72a5614
larunnel161@gmail.com <larunnel161@gmail.com>
2024-01-27 22:33:06 -0600

    Documenting Bernouli encoding for the sentence dataset

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from preprocess.getMNIST import getMNIST
-from preprocess.getPolitical import getPolSentences
+from preprocess.getPolitical import getPolSentences_Bernouli
 def main():
     # (X, Y) = getMNIST()
-    getPolSentences()
+    (X, Y) = getPolSentences_Bernouli()
     print("Hello World")
     
 if __name__ == '__main__':
