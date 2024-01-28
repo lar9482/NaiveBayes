8cea65ffe2a8571c315671cf1b39f86aac523d67
lar9482 <lar9482@utulsa.edu>
2024-01-27 22:33:06 -0600

    Documenting Bernouli encoding for the sentence dataset

--- preprocess/getPolitical.py
+++ preprocess/getPolitical.py
@@ -1,33 +1,44 @@
 import numpy as np
 
-def getPolSentences(numInstances = 1000, booleanize = False):
+def getPolSentences_Bernouli(numInstances = 2):
+    """
+        Encoding Scheme:
+        X:
+        Given the sentence, "Tulsa University", with the words begin positioned as the 10th and 11th words in the dictionary(sorted),
+
+        then the sentence encoded as [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0,,,0] 
+        with placeholder zeros for the rest of the dictionary.
+
+        Y:
+        Liberal is assigned 0
+        Conservative is assigned 1.
+    """
     numInstancesLiberal = numInstances // 2
     numInstancesConservative = numInstances - numInstancesLiberal
 
     liberalSentences = getSentences(numInstancesLiberal, True)
     conservativeSentences = getSentences(numInstancesConservative, False)
-
     vocab = getVocabulary(liberalSentences, conservativeSentences)
 
     X = np.zeros((len(liberalSentences) + len(conservativeSentences), len(vocab)), dtype=float)
     Y = np.zeros((len(liberalSentences) + len(conservativeSentences), 1), dtype=float)
 
     for i in range(0, len(liberalSentences)):
-        words = liberalSentences[i].split()
-        if (booleanize):
-            words = list(set(words))
+        words = list(set(
+            liberalSentences[i].split()
+        ))
         
         for word in words:
-            X[i][vocab.index(word)] += 1
+            X[i][vocab.index(word)] = 1
             Y[i] = 0
     
     for i in range(0, len(conservativeSentences)):
-        words = conservativeSentences[i].split()
-        if (booleanize):
-            words = list(set(words))
+        words = list(set(
+            conservativeSentences[i].split()
+        ))
         
         for word in words:
-            X[i + len(liberalSentences)][vocab.index(word)] += 1
+            X[i + len(liberalSentences)][vocab.index(word)] = 1
             Y[i + len(liberalSentences)] = 1
     
     return (X, Y)
