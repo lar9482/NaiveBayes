5ca8033ac12f62843f7ea80a4530b4b337fcdf9d
larunnel161@gmail.com <larunnel161@gmail.com>
2024-01-28 22:43:41 -0600

    Adjusting laplace smoothing to match Andrew Ng more

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from NFold import NFold
 from model.NaiveBayes.BernoulliNB import BernoulliNB
 
 def main():
-    (X, Y) = getPolSentences_Bernoulli(1000)
+    (X, Y) = getPolSentences_Bernoulli(7500)
     (X, Y) = shuffleDataset(X, Y)
-    model = BernoulliNB(len(X[0]), 2, 0)
-    NFold(X, Y, model)
+    model = BernoulliNB(len(X[0]), 2, 1)
+    (trainAcc, testAcc) = NFold(X, Y, model)
+    print(trainAcc, testAcc)
     
 if __name__ == '__main__':
     main()
