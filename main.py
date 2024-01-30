1e55211504464db4d26c176c6973eb372340f7a2
lar9482 <lar9482@utulsa.edu>
2024-01-29 18:09:36 -0600

    Wrote draft for the training routine of the multinomial model

--- main.py
+++ main.py
@@ -4,13 +4,21 @@ from preprocess.shuffle import shuffleDataset
 from NFold import NFold
 
 from model.NaiveBayes.BernoulliNB import BernoulliNB
+from model.NaiveBayes.MultinomialNB import MultinomialNB
 
 def main():
-    (X, Y) = getPolSentences_Bernoulli(1000)
+    # (X, Y) = getPolSentences_Bernoulli(1000)
+    # (X, Y) = shuffleDataset(X, Y)
+    # model = BernoulliNB(len(X[0]), 2, 0)
+    # (trainAcc, testAcc) = NFold(X, Y, model)
+    # print(trainAcc, testAcc)
+
+    (X, Y, numVocab, numClasses) = getPolSentences_Multinomial(10)
     (X, Y) = shuffleDataset(X, Y)
-    model = BernoulliNB(len(X[0]), 2, 0)
-    (trainAcc, testAcc) = NFold(X, Y, model)
+    multiNB_pol = MultinomialNB(numVocab, numClasses, 0)
+    (trainAcc, testAcc) = NFold(X, Y, multiNB_pol)
     print(trainAcc, testAcc)
+    print()
     
 if __name__ == '__main__':
     main()
